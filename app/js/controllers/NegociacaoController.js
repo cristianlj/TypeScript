db2826cb34460896661d5841480d83a4761f8d3c
CRISTIAN LEANDRO DE JESUS <39355375+cristianlj@users.noreply.github.com>
2019-12-19 16:32:17 -0300

    WIP: type Script

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -1,18 +1,92 @@
-class NegociacaoController {
-    constructor() {
-        this._negociacoes = new Negociacoes();
-        this._negociacoesView = new NegociacoesView('#negociacoesView');
-        this._mensagemView = new MensagemView('#mensagemView');
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
-        this._negociacoesView.update(this._negociacoes);
-    }
-    adiciona(event) {
-        event.preventDefault();
-        const negociacao = new Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
-        this._negociacoes.adiciona(negociacao);
-        this._negociacoesView.update(this._negociacoes);
-        this._mensagemView.update('Negociação adicionada com sucesso!');
-    }
-}
+System.register(["../views/index", "../models/index", "../helpers/decorators/index"], function (exports_1, context_1) {
+    "use strict";
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __moduleName = context_1 && context_1.id;
+    var index_1, index_2, index_3, timer, NegociacaoController, diaDaSemana;
+    return {
+        setters: [
+            function (index_1_1) {
+                index_1 = index_1_1;
+            },
+            function (index_2_1) {
+                index_2 = index_2_1;
+            },
+            function (index_3_1) {
+                index_3 = index_3_1;
+            }
+        ],
+        execute: function () {
+            timer = 0;
+            NegociacaoController = class NegociacaoController {
+                constructor() {
+                    this._negociacoes = new index_2.Negociacoes();
+                    this._negociacoesView = new index_1.NegociacoesView('#negociacoesView');
+                    this._mensagemView = new index_1.MensagemView('#mensagemView');
+                    this._negociacoesView.update(this._negociacoes);
+                }
+                adiciona(event) {
+                    event.preventDefault();
+                    let data = new Date(this._inputData.val().replace(/-/g, ','));
+                    if (!this._ehDiaUtil(data)) {
+                        this._mensagemView.update('Negociações somente em dias úteis!');
+                        return;
+                    }
+                    const negociacao = new index_2.Negociacao(data, parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
+                    this._negociacoes.adiciona(negociacao);
+                    this._negociacoesView.update(this._negociacoes);
+                    this._mensagemView.update('Negociação adicionada com sucesso!');
+                }
+                _ehDiaUtil(data) {
+                    return data.getDay() != diaDaSemana.Sabado && data.getDay() != diaDaSemana.Domingo;
+                }
+                importaDados() {
+                    function isOk(res) {
+                        if (res.ok) {
+                            return res;
+                        }
+                        else {
+                            throw new Error(res.statusText);
+                        }
+                    }
+                    clearTimeout(timer);
+                    timer = setTimeout(() => {
+                        fetch('http://localhost:8080/dados')
+                            .then(res => isOk(res))
+                            .then(res => res.json())
+                            .then((dados) => {
+                            dados
+                                .map(dado => new index_2.Negociacao(new Date(), dado.vezes, dado.montante))
+                                .forEach(negociacao => this._negociacoes.adiciona(negociacao));
+                            this._negociacoesView.update(this._negociacoes);
+                        })
+                            .catch(err => console.log(err.message));
+                    }, 500);
+                }
+            };
+            __decorate([
+                index_3.domInject('#data')
+            ], NegociacaoController.prototype, "_inputData", void 0);
+            __decorate([
+                index_3.domInject('#quantidade')
+            ], NegociacaoController.prototype, "_inputQuantidade", void 0);
+            __decorate([
+                index_3.domInject('#valor')
+            ], NegociacaoController.prototype, "_inputValor", void 0);
+            exports_1("NegociacaoController", NegociacaoController);
+            (function (diaDaSemana) {
+                diaDaSemana[diaDaSemana["Domingo"] = 0] = "Domingo";
+                diaDaSemana[diaDaSemana["Segunda"] = 1] = "Segunda";
+                diaDaSemana[diaDaSemana["Ter\u00E7a"] = 2] = "Ter\u00E7a";
+                diaDaSemana[diaDaSemana["Quarta"] = 3] = "Quarta";
+                diaDaSemana[diaDaSemana["Quinta"] = 4] = "Quinta";
+                diaDaSemana[diaDaSemana["Sexta"] = 5] = "Sexta";
+                diaDaSemana[diaDaSemana["Sabado"] = 6] = "Sabado";
+            })(diaDaSemana || (diaDaSemana = {}));
+        }
+    };
+});
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,18 +1,28 @@
-abstract class View<T> {
-//Coments
-    protected _elemento: JQuery;
+import {LogarTempoDeExecucao }from '../helpers/decorators/index';
+export abstract class View<T> {
 
-    constructor(seletor: string) {
+    protected _elemento: JQuery;
+    private _escapar: boolean;
+
+    constructor(seletor: string, escapar: boolean = false) {
 
         this._elemento = $(seletor);
+        this._escapar = escapar;
     }
 
+    @LogarTempoDeExecucao(true)
     update(model: T) {
 
-        this._elemento.html(this.template(model));
+
+        let template = this.template(model);
+        if (this._escapar)
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+
+        this._elemento.html(template);
+
+
     }
 
     abstract template(model: T): string;
 
 }
-
